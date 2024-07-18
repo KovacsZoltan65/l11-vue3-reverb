8fa9c20380cf39e5055cfb654beef75e84b2b255
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2024-07-18 12:37:40 +0200

    mentés

---
+++ app/Models/Project.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Str;
+use App\Support\Generators;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    const NOT_STARTED = 0;
+    const PENDING = 1;
+    const COMPLETED = 2;
+    
+    protected $guarded = [];
+
+    public static function createSlug($name)
+    {
+        $code = Generators::generateRandomCode();
+        $slug=Str::slug($name) . '-' . $code;
+        
+        return $slug;
+    }
+    
+    public function task_progress()
+    {
+        return $this->hasOne(TaskProgress::class,'projectId');
+    }
+    
+    public function tasks()
+    {
+        return $this->hasMany(Task::class,'projectId');
+    }
+}
