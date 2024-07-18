bd59f705f82d075f7493afe9ded47322b5727673
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2024-07-18 10:33:56 +0200

    mentés

--- app/Events/NewUserCreated.php
+++ app/Events/NewUserCreated.php
@@ -8,14 +8,38 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
+/**
+ * Class NewUserCreated
+ *
+ * @package App\Events
+ *
+ * @property-read User $user
+ */
 class NewUserCreated
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    /**
+     * The user that was created
+     *
+     * @var User
+     */
     public $user;
 
     /**
+     * The channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('users');
+    }
+
+    /**
      * Create a new event instance.
+     *
+     * @param User $user
      */
     public function __construct(User $user)
     {
@@ -27,10 +51,16 @@ class NewUserCreated
      *
      * @return array<int, \Illuminate\Broadcasting\Channel>
      */
-    public function broadcastOn(): array
+    /*
+     public function broadcastOn(): array
     {
+        // The event should be broadcasted on a private channel named 'channel-name'.
+        // The channel is created using the 'PrivateChannel' class.
+        // This channel is used to notify the user about the newly created account.
+        // The 'channel-name' is the name of the channel. Replace it with the actual name of the channel.
         return [
             new PrivateChannel('channel-name'),
         ];
     }
+    */
 }
--- app/Listeners/SendEmailVerification.php
+++ app/Listeners/SendEmailVerification.php
@@ -9,7 +9,15 @@ use App\Events\NewUserCreated;
 use App\Mail\SendMail;
 use Illuminate\Support\Facades\Mail;
 
-class SendEmailVerification
+/**
+ * Send email verification after user is created.
+ *
+ * This listener will send the user an email verification after the user is created.
+ *
+ * @package App\Listeners
+ * @author Ezequiel H. B. <ezequielhectorb@gmail.com>
+ */
+class SendEmailVerification implements ShouldQueue
 {
     /**
      * Create the event listener.
@@ -21,9 +29,17 @@ class SendEmailVerification
 
     /**
      * Handle the event.
+     *
+     * This method is responsible for sending an email verification
+     * to the newly created user.
+     *
+     * @param NewUserCreated $event The event containing the user data.
+     * @return void
      */
     public function handle(NewUserCreated $event): void
     {
+        sleep(5);
+        // Send an email to the user's email address using the SendMail mailable.
         Mail::to($event->user->email)
             ->send(new SendMail($event->user));
     }
